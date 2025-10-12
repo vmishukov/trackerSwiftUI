7cb14d9940c505ca4138dcc0493bef5ac61b086c
ketamine <vladg3@mail.ru>
2025-10-12 17:49:42 +0700

    Initial commit

---
+++ TrackerSwiftUI/TrackerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerSwiftUIApp.swift
+//  TrackerSwiftUI
+//
+//  Created by Vladislav Mishukov on 12.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct TrackerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
