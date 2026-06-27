fc6d9f9b65d299f9687bf2c2b2795b88adaa50a1
ketamine <vladg3@mail.ru>
2026-06-27 18:23:20 +0700

    Feature: added  tracker category swift data logic

--- TrackerSwiftUI/Views/AddCategory/ViewModel/AddCategoryViewModel.swift
+++ TrackerSwiftUI/Views/AddCategory/ViewModel/AddCategoryViewModel.swift
@@ -7,10 +7,16 @@
 
 import Combine
 import SwiftUI
+import SwiftData
 
 final class AddCategoryViewModel: ObservableObject {
- 
+    
     @Published var newCategoryIsPresented: Bool = false
+    @Published var editCategoryIsPresented: Bool = false
     
+    var trackerCategoryToEdit: TrackerCategory?
     
+    func removeTrackerCategory(modelContext: ModelContext, model: TrackerCategory) {
+        modelContext.delete(model)
+    }
 }
